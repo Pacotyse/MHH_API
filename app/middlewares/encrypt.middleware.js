9073020f881d840d53f6ae79577515af99ab3421
Pacotyse <e.tison85@gmail.com>
2023-07-28 22:50:17 +0200

    REFACTOR : Adding documentation and refactoring code of encrypt.middleware.js

--- app/middlewares/encrypt.middleware.js
+++ app/middlewares/encrypt.middleware.js
@@ -1,16 +1,33 @@
 import bcrypt from "bcrypt";
 
-const saltRounds = 10;
+/**
+ * Middleware for encrypting the user's password.
+ * Retrieves the password from the request body
+ * and encrypts it before storing it under the same key "password"
+ * in the request. The encrypted password is ready to be securely
+ * saved in the database.
+ *
+ * @middleware
+ * @function password
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {Function} next - The next middleware function.
+ * @throws {Error} An error may be thrown if encryption fails.
+ * @returns {void}
+ */
 
 async function password(req, res, next) {
-  const { password } = req.body;
   try {
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.BCRYPT_SECRET)
+    );
     req.body.password = hashedPassword;
     next();
   } catch (error) {
     res.status(500).send({
-      message: "Erreur lors du hachage du mot de passe.",
+      message: "An error has occured ",
     });
   }
 }
