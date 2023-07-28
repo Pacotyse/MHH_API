c5e959b9714daf6c25a93100645fd47a43a46c62
Pacotyse <e.tison85@gmail.com>
2023-07-28 23:06:23 +0200

    REFACTOR : HTTP POST /users to HTTP POST /Register

--- app/routers/api.router.js
+++ app/routers/api.router.js
@@ -1,9 +1,13 @@
-import express from 'express';
+import express from "express";
+import bcrypt from "../middlewares/encrypt.middleware.js";
+import apiController from "../controllers/api.controller.js";
 
-import userRouter from './user.router.js'
+import userRouter from "./user.router.js";
 
 const router = express.Router();
 
-router.use('/users', userRouter);
+router.use("/users", userRouter);
+
+router.route("/register").post(bcrypt, apiController.user.createOne);
 
 export default router;
--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -1,14 +1,11 @@
-import express from 'express';
-import apiController from '../controllers/api.controller.js'
-import bcrypt from '../middlewares/encrypt.middleware.js';
+import express from "express";
+import apiController from "../controllers/api.controller.js";
 
 const router = express.Router();
 
-router.route('/')
-  .get(apiController.user.getAll)
-  .post(bcrypt, apiController.user.createOne);
+router.route("/").get(apiController.user.getAll);
 
-router.route('/:id')
+router.route("/:id")
   .get(apiController.user.getOne)
   .put(apiController.user.updateOne)
   .delete(apiController.user.deleteOne);
