fb2c9ea57f6f36a20bdfb3cc376d77ba48acec70
Pacotyse <e.tison85@gmail.com>
2023-09-22 13:35:15 +0200

    ADD: Implement the 'findBy' method

--- app/models/core.dataMapper.js
+++ app/models/core.dataMapper.js
@@ -1,125 +1,160 @@
 class CoreDatamapper {
-    tableName;
-  
-    constructor(client) {
-      this.client = client;
-    }
-  
-    /**
-           * Récupération par identifiant
-           * @param {number} id identifiant
-           * @returns {object} un enregistrement
-           */
-  
-    async findByPk(id) {
-      const preparedQuery = {
-        text: `SELECT * FROM "${this.tableName}" WHERE id = $1`,
-        values: [id],
-      };
-  
-      const result = await this.client.query(preparedQuery);
-  
-      if (!result.rows[0]) {
-        return null;
-      }
-  
-      return result.rows[0];
-    }
-  
-    /**
-           * Permet de récupérer l'ensemble des enregistrement
-           * d'une table ou une liste d'enregistrement.
-           * @returns {object[]} une liste d'enregistrements
-           */
-  
-    async findAll() {
-      const result = await this.client.query(`SELECT * FROM "${this.tableName}"`);
-  
-      return result.rows;
+  tableName;
+
+  constructor(client) {
+    this.client = client;
+  }
+
+  /**
+   * Récupération par identifiant
+   * @param {number} id identifiant
+   * @returns {object} un enregistrement
+   */
+
+  async findByPk(id) {
+    const preparedQuery = {
+      text: `SELECT * FROM "${this.tableName}" WHERE id = $1`,
+      values: [id],
+    };
+
+    const result = await this.client.query(preparedQuery);
+
+    if (!result.rows[0]) {
+      return null;
     }
 
-    /**
-           * Insertion de données dans la table
-           * @param {object} inputData données à insérer dans la table
-           * @returns {object} l'enregistrement créé
-           */
-    async create(inputData) {
-      const fields = [];
-      const placeholders = [];
-      const values = [];
-      let indexPlaceholder = 1;
-  
-      Object.entries(inputData).forEach(([prop, value]) => {
-        fields.push(`"${prop}"`);
-        placeholders.push(`$${indexPlaceholder}`);
-        indexPlaceholder += 1;
-        values.push(value);
-      });
-  
+    return result.rows[0];
+  }
+
+  /**
+   * Find a record in the database table based on a specified field and its value.
+   *
+   * @async
+   * @function findBy
+   * @param {string} field - The name of the field to search for.
+   * @param {any} value - The value to match against in the specified field.
+   * @returns {Promise<Object|null>} A Promise that resolves with the found record as an object, or null if no record is found.
+   * @throws {Error} Throws an error if there's a problem with the database query.
+   */
+  async findBy(field, value) {
+    try {
+      // Prepare the SQL query
       const preparedQuery = {
-        text: `
+        text: `SELECT * FROM "${this.tableName}" WHERE "${field}" = $1`,
+        values: [value],
+      };
+
+      // Execute the query
+      const result = await this.client.query(preparedQuery);
+
+      // Check if a record was found
+      if (!result.rows[0]) {
+        return null; // No record found
+      }
+
+      return result.rows[0]; // Return the found record as an object
+    } catch (error) {
+      throw new Error("Database query error: " + error.message);
+    }
+  }
+
+  /**
+   * Permet de récupérer l'ensemble des enregistrement
+   * d'une table ou une liste d'enregistrement.
+   * @returns {object[]} une liste d'enregistrements
+   */
+
+  async findAll() {
+    const result = await this.client.query(`SELECT * FROM "${this.tableName}"`);
+
+    return result.rows;
+  }
+
+  /**
+   * Insertion de données dans la table
+   * @param {object} inputData données à insérer dans la table
+   * @returns {object} l'enregistrement créé
+   */
+  async create(inputData) {
+    const fields = [];
+    const placeholders = [];
+    const values = [];
+    let indexPlaceholder = 1;
+
+    Object.entries(inputData).forEach(([prop, value]) => {
+      fields.push(`"${prop}"`);
+      placeholders.push(`$${indexPlaceholder}`);
+      indexPlaceholder += 1;
+      values.push(value);
+    });
+
+    const preparedQuery = {
+      text: `
               INSERT INTO "${this.tableName}"
               (${fields})
               VALUES (${placeholders})
               RETURNING *
             `,
-        values,
-      };
-  
-      const result = await this.client.query(preparedQuery);
-      const row = result.rows[0];
-  
-      return row;
-    }
-  
-    /**
-           * Modification de données dans la table
-           * @param {object} param0 données à mettre à jour dans la table comprenant également
-           * l'identifiant de l'enregistrement
-           * @returns {object} l'enregistrement mis à jour
-           */
-  
-    async update({ id, ...inputData }) {
-      const fieldsAndPlaceholders = [];
-      let indexPlaceholder = 1;
-      const values = [];
-  
-      Object.entries(inputData).forEach(([prop, value]) => {
-        fieldsAndPlaceholders.push(`"${prop}" = $${indexPlaceholder}`);
-        indexPlaceholder += 1;
-        values.push(value);
-      });
-  
-      values.push(id);
-  
-      const preparedQuery = {
-        text: `
+      values,
+    };
+
+    const result = await this.client.query(preparedQuery);
+    const row = result.rows[0];
+
+    return row;
+  }
+
+  /**
+   * Modification de données dans la table
+   * @param {object} param0 données à mettre à jour dans la table comprenant également
+   * l'identifiant de l'enregistrement
+   * @returns {object} l'enregistrement mis à jour
+   */
+
+  async update({ id, ...inputData }) {
+    const fieldsAndPlaceholders = [];
+    let indexPlaceholder = 1;
+    const values = [];
+
+    Object.entries(inputData).forEach(([prop, value]) => {
+      fieldsAndPlaceholders.push(`"${prop}" = $${indexPlaceholder}`);
+      indexPlaceholder += 1;
+      values.push(value);
+    });
+
+    values.push(id);
+
+    const preparedQuery = {
+      text: `
               UPDATE "${this.tableName}" SET
               ${fieldsAndPlaceholders},
               updated_at = now()
               WHERE id = $${indexPlaceholder}
               RETURNING *
             `,
-        values,
-      };
-  
-      const result = await this.client.query(preparedQuery);
-      const row = result.rows[0];
-  
-      return row;
-    }
-  
-    /**
-           * Suppression d'un enregistrement
-           * @param {number} id
-           * @returns {boolean} nombre d'enregistrement supprimés
-          */
-  
-    async delete(id) {
-      const result = await this.client.query(`DELETE FROM "${this.tableName}" WHERE id = $1`, [id]);
-      // !! cast un falsy en false
-      return !!result.rowCount;
-    }
-  };
+      values,
+    };
+
+    const result = await this.client.query(preparedQuery);
+    const row = result.rows[0];
+
+    return row;
+  }
+
+  /**
+   * Suppression d'un enregistrement
+   * @param {number} id
+   * @returns {boolean} nombre d'enregistrement supprimés
+   */
+
+  async delete(id) {
+    const result = await this.client.query(
+      `DELETE FROM "${this.tableName}" WHERE id = $1`,
+      [id]
+    );
+    // !! cast un falsy en false
+    return !!result.rowCount;
+  }
+}
 
 export default CoreDatamapper;
