799aca1d3ce6e6d8d7e3281177e5588e5aae3ff0
Pacotyse <e.tison85@gmail.com>
2023-09-22 15:07:14 +0200

    DOCUMENT: Prettier documentation

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -14,15 +14,23 @@ const userController = {
    */
   async getOne(req, res) {
     try {
+      // Extract the user ID from the request parameters
       const { id } = req.params;
+
+      // Retrieve the user data by ID using the API model
       const data = await apiModel.user.findByPk(id);
+
+      // Check if the user data is found
       if (!data) {
         return res.status(404).json({ error: "User not found." });
-      };
+      }
+
+      // Send the user data as a JSON response
       res.json(data);
     } catch (error) {
+      // Handle any internal server errors with a 500 Internal Server Error response
       res.status(500).json({ error: "Internal server error" });
-    };
+    }
   },
 
   /**
@@ -40,20 +48,27 @@ const userController = {
    */
   async createOne(req, res) {
     try {
-      console.log(req.body);
+      // Extract email, password, and username from the request body
       const { email, password, username } = req.body;
+
+      // Check if any of the required fields are missing
       if (!email || !password || !username) {
         return res.status(400).json({ error: "Missing required fields." });
-      };
+      }
+
+      // Create a new user using the API model
       const newUser = await apiModel.user.create({
         email,
         password,
         username,
       });
+
+      // Send the new user data as a JSON response with a 201 Created status code
       res.status(201).json(newUser);
     } catch (error) {
+      // Handle any internal server errors with a 500 Internal Server Error response
       res.status(500).json({ error: "Internal server error" });
-    };
+    }
   },
 
   /**
@@ -71,21 +86,27 @@ const userController = {
    */
   async updateOne(req, res) {
     try {
+      // Extract user_id from the request parameters and input data from the request body
       const user_id = req.params.id;
       const inputData = req.body;
 
-      if (inputData.id) {
+      // Check if 'id' field is included in the updated data (forbidden field)
+      if (inputData.id) {
         return res.status(403).json({ error: "Forbidden field" });
-      };
-      
+      }
+
+      // Update the existing user with the provided ID using the API model
       const updatedUser = await apiModel.user.update({
         id: user_id,
         ...inputData,
       });
+
+      // Send the updated user data as a JSON response with a 201 Created status code
       res.status(201).json(updatedUser);
     } catch (error) {
+      // Handle any internal server errors with a 500 Internal Server Error response
       res.status(500).json({ error: "Internal server error" });
-    };
+    }
   },
 
   /**
@@ -102,12 +123,18 @@ const userController = {
    */
   async deleteOne(req, res) {
     try {
+      // Extract user_id from the request parameters
       const user_id = req.params.id;
+
+      // Delete the user with the provided ID using the API model
       const deletedData = await apiModel.user.delete(user_id);
+
+      // Send a 204 No Content response to indicate successful deletion
       res.status(204).json(deletedData);
     } catch (error) {
+      // Handle any internal server errors with a 500 Internal Server Error response
       res.status(500).json({ error: "Internal server error" });
-    };
+    }
   },
 };
 
