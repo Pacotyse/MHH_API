066dc492f9d7a2009f2a686c7286f1ec0fdf075c
Pacotyse <e.tison85@gmail.com>
2023-10-31 12:11:25 +0100

    Adding new model armor

--- app/models/api.dataMapper.js
+++ app/models/api.dataMapper.js
@@ -2,11 +2,13 @@ import client from './pg.client.js';
 import User from './user.dataMapper.js';
 import Loadout from './loadout.dataMapper.js';
 import Weapon from './weapon.dataMapper.js';
+import Armor from './armor.dataMapper.js';
 
 const apiModel = {
   user: new User(client),
   loadout: new Loadout(client),
-  weapon: new Weapon(client)
+  weapon: new Weapon(client),
+  armor: new Armor(client),
 }
 
 export default apiModel;
