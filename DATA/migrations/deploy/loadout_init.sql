2e3376ebad140af67264cf1d2a9ad7f346421e7c
Pacotyse <e.tison85@gmail.com>
2023-10-31 13:23:36 +0100

    Changing order of the columns for more readability

--- DATA/migrations/deploy/loadout_init.sql
+++ DATA/migrations/deploy/loadout_init.sql
@@ -4,12 +4,12 @@ BEGIN;
 
 CREATE TABLE "loadout" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ
     "user_id" INTEGER NOT NULL REFERENCES "user"("id") ON DELETE CASCADE,
     "name" TEXT NOT NULL,
     "description" TEXT,
     "weapon" JSON NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-    "updated_at" TIMESTAMPTZ
 );
 
 COMMIT;
--- DATA/migrations/deploy/user_init.sql
+++ DATA/migrations/deploy/user_init.sql
@@ -4,15 +4,15 @@ BEGIN;
 
 CREATE TABLE "user" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
+    "updated_at" TIMESTAMPTZ,
     "email" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL,
     "username" TEXT NOT NULL UNIQUE,
     "steam_id" TEXT,
     "playstation_id" TEXT,
     "xbox_id" TEXT,
-    "switch_id" TEXT,
-    "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
-    "updated_at" TIMESTAMPTZ
+    "switch_id" TEXT
 );
 
 COMMIT;
