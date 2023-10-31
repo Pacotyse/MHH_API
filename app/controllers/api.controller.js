e834c9b4e1a6a989eab8505446b81548a721953d
Pacotyse <e.tison85@gmail.com>
2023-10-31 09:28:47 +0100

    Adding loadout's controllers GET, POST, PUT & DEL with token verification

--- app/controllers/api.controller.js
+++ app/controllers/api.controller.js
@@ -1,8 +1,10 @@
 import userController from './user.controller.js';
+import loadoutController from './loadout.controller.js';
 import weaponController from './weapon.controller.js';
 
 const apiController = {
   user: userController,
+  loadout: loadoutController,
   weapon: weaponController
 }
 
